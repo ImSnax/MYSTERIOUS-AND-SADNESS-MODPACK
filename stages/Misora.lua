198153b349af22524e82bdfddaaafc5bf45809e3
Daewron01 <daewron.10@gmail.com>
2026-05-01 17:06:16 -0500

    Acomodando para el nuevo Bg de Misora

--- stages/Misora.lua
+++ stages/Misora.lua
@@ -1,8 +1,8 @@
 
 function onCreate()
 
-    makeLuaSprite('bg', 'misoraStage/bg', -450, -200);
-    scaleObject('bg', 1.7, 1.7);
-    addLuaSprite('bg', false);
+    makeLuaSprite('Bg', 'misoraStage/Bg', -910, -500);
+    scaleObject('Bg', 2.2, 2.2);
+    addLuaSprite('Bg', false);
 
 end
