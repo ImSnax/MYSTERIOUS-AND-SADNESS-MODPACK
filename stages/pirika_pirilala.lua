c8b8aea2a4f046d07b7b3bc6939a37b9e1c84aa8
Daewron01 <daewron.10@gmail.com>
2026-05-02 12:40:49 -0500

    New stage

--- stages/pirika_pirilala.lua
+++ stages/pirika_pirilala.lua
@@ -1,11 +1,21 @@
 function onCreate()
-	makeLuaSprite('BG_pirika_pirilala', 'pirika_pirilalaStage/BG_pirika_pirilala', -700, -440);
-	setLuaSpriteScrollFactor('BG_pirika_pirilala', 1.2, 1.2);
-	scaleObject('BG_pirika_pirilala', 3, 3);
+	makeLuaSprite('bg', 'pirika_pirilalaStage/bg', -1200, -900);
+	setLuaSpriteScrollFactor('bg', 1.3, 1.3);
+	scaleObject('bg', 4, 4);
 	
-	makeLuaSprite('escobas', 'pirika_pirilalaStage/escobas', -540, -240);
-	scaleObject('escobas', 2.7, 2.7);
+	makeLuaSprite('rocas2', 'pirika_pirilalaStage/rocas2', 900, -50);
+	setLuaSpriteScrollFactor('rocas2', 1.2, 1.2);
+	scaleObject('rocas2', 1.7, 1.7);
 	
-	addLuaSprite('BG_pirika_pirilala', false);
-	addLuaSprite('escobas', false)
+	makeLuaSprite('rocas1', 'pirika_pirilalaStage/rocas1', -450, -250);
+	setLuaSpriteScrollFactor('rocas1', 1.1, 1.1);
+	scaleObject('rocas1', 2.3, 2.3);
+	
+	makeLuaSprite('suelo', 'pirika_pirilalaStage/suelo', -900, -850);
+	scaleObject('suelo', 3.4, 3.4);
+	
+	addLuaSprite('bg', false);
+	addLuaSprite('rocas2', false)
+	addLuaSprite('rocas1', false)
+	addLuaSprite('suelo', false)
 end
