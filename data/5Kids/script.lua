9ba73cf18ecb338554a3cdd116058340b437d414
Daewron01 <daewron.10@gmail.com>
2026-05-26 12:15:09 -0500

    Add files via upload

--- data/5Kids/script.lua
+++ data/5Kids/script.lua
@@ -1,13 +1,28 @@
 local allowCountdown = false
+
 function onStartCountdown()
-	if not allowCountdown and isStoryMode and not seenCutscene then --Block the first countdown
-		startVideo('week2_1');
-		allowCountdown = true;
-		return Function_Stop;
-	end
-	return Function_Continue;
+    if not allowCountdown and not seenCutscene then
+        startVideo('week2_1')
+        allowCountdown = true
+        return Function_Stop
+    end
+    return Function_Continue
 end
 
 function onCreate()
+
     setProperty('skipCountdown', true)
+    
+    makeLuaSprite('logo', '5kidsStage/4kids', 0, 0) 
+    setObjectCamera('logo', 'other')
+    addLuaSprite('logo', true)
+    scaleObject('logo', 0.2, 0.2)
+
+    if downscroll then
+        setProperty('logo.y', 40) 
+    else
+        setProperty('logo.y', 610)
+    end
+
+    setProperty('logo.x', screenWidth / 1.035 - getProperty('logo.width') / 1.035)
 end
