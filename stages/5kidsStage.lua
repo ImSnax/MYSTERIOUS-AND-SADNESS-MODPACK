90803b98a9f8f40242ec261d83911fc4c16b82e3
Daewron01 <daewron.10@gmail.com>
2026-09-23 12:33:29 -0500

    Fix path for 'ns' Lua sprite

--- stages/5kidsStage.lua
+++ stages/5kidsStage.lua
@@ -2,7 +2,7 @@ function onCreate()
     makeLuaSprite('tienda', 'stages/5kidsStage/tienda', -450, -325);
     scaleObject('tienda', 3, 3);
     
-    makeLuaSprite('ns', 'doremi/5kidsStage/ns', 0, 0);
+    makeLuaSprite('ns', 'stages/5kidsStage/ns', 0, 0);
     setObjectCamera('ns', 'hud')
     
     addLuaSprite('tienda', false);
