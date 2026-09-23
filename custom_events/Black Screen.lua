77038a27e0cd5ef0ce9cba8f412b58cd4633fc68
Daewron01 <daewron.10@gmail.com>
2026-09-23 11:33:54 -0500

    Add files via upload

--- custom_events/Black Screen.lua
+++ custom_events/Black Screen.lua
@@ -1,35 +1,46 @@
 function onCreate()
-    makeLuaSprite('blackOverlay', '', -1000, -1000)
-    makeGraphic('blackOverlay', 4000, 4000, '000000')
-    
-    setObjectCamera('blackOverlay', 'game')
-    setScrollFactor('blackOverlay', 0, 0)
-    
-    setProperty('blackOverlay.alpha', 0)
-    
-    addLuaSprite('blackOverlay', true)
+    makeLuaSprite('blackScreen', '', 0, 0)
+    makeGraphic('blackScreen', screenWidth, screenHeight, '000000')
+
+    setObjectCamera('blackScreen', 'hud')
+    setObjectOrder('blackScreen', getObjectOrder('healthBar') - 1)
+
+    setProperty('blackScreen.alpha', 0)
+    addLuaSprite('blackScreen', true)
 end
 
 function onEvent(name, value1, value2)
     if name == 'Black Screen' then
-        
-        local estado = string.lower(value1)
-        local duracion = tonumber(value2)
-        if duracion == nil then
-            duracion = 0 
+        local mode = value1
+        local speed = 0
+        local commaIndex = string.find(value1, ",")
+        if commaIndex ~= nil then
+            mode = string.sub(value1, 1, commaIndex - 1)
+            speed = tonumber(string.sub(value1, commaIndex + 1)) or 0
         end
         
-        local targetAlpha = 0
-        if estado == 'on' then
-            targetAlpha = 1
-        elseif estado == 'off' then
-            targetAlpha = 0
-        end
-        
-        if duracion > 0 then
-            doTweenAlpha('fadeNegroTween', 'blackOverlay', targetAlpha, duracion, 'linear')
+        mode = string.gsub(mode, "^%s*(.-)%s*$", "%1")
+
+        if value2 == 'other' then
+            setObjectCamera('blackScreen', 'other')
+            setObjectOrder('blackScreen', 0)
         else
-            setProperty('blackOverlay.alpha', targetAlpha)
+            setObjectCamera('blackScreen', 'hud')
+            setObjectOrder('blackScreen', getObjectOrder('healthBar') - 1)
+        end
+
+        if mode == 'on' then
+            if speed > 0 then
+                doTweenAlpha('blackFadeIn', 'blackScreen', 1, speed, 'linear')
+            else
+                setProperty('blackScreen.alpha', 1)
+            end
+        elseif mode == 'off' or mode == '' then
+            if speed > 0 then
+                doTweenAlpha('blackFadeOut', 'blackScreen', 0, speed, 'linear')
+            else
+                setProperty('blackScreen.alpha', 0)
+            end
         end
     end
 end
